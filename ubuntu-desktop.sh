0cf4cc5634fe5d90089c17c947981ff631b07b74
elppans <elppans@gmail.com>
2026-08-19 22:50:06 -0300

    Adicionado comandos

--- ubuntu-desktop.sh
+++ ubuntu-desktop.sh
@@ -4,9 +4,9 @@
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup_"$(date +%Y%m%d%H%M)"
 
 cat <<EOF | sudo tee /etc/apt/sources.list
-deb http://br.archive.ubuntu.com/ubuntu/ jammy main #universe restricted
-deb http://br.archive.ubuntu.com/ubuntu/ jammy-updates main #universe restricted
-deb http://br.archive.ubuntu.com/ubuntu/ jammy-security main #universe restricted
+deb http://br.archive.ubuntu.com/ubuntu/ jammy main universe restricted
+deb http://br.archive.ubuntu.com/ubuntu/ jammy-updates main universe restricted
+deb http://br.archive.ubuntu.com/ubuntu/ jammy-security main universe restricted
 EOF
 
 sudo apt update
@@ -17,3 +17,9 @@ sudo systemctl set-default graphical.target
 sudo apt install -f
 sudo systemctl start gdm3
 sudo apt upgrade -y
+sudo apt clean
+sudo apt autoclean
+sudo apt autoremove
+
+sleep 15
+sudo systemctl reboot -i
