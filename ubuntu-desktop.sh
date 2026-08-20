5497dfda58ecdaceb1353507b48d856987654c25
elppans <elppansmk@gmail.com>
2026-08-20 10:23:19 -0300

    Implement backup for sources.list.d and update sources.list
    Added backup functionality for sources.list.d directory and updated sources.list.

--- ubuntu-desktop.sh
+++ ubuntu-desktop.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 
+# -- Diretório sources.list.d --
+#!/bin/bash
 
+DIR="/etc/apt/sources.list.d"
+BACKUP_DIR="/etc/apt/sources.list.d-backup-$(date +%Y%m%d%H%M%S)"
+
+# Verifica se o diretório existe e tem conteúdo
+if [ -d "$DIR" ] && [ "$(ls -A $DIR)" ]; then
+    echo "Fazendo backup do diretório \"$DIR\"..."
+    
+    # Cria o diretório de backup
+    mkdir -p "$BACKUP_DIR"
+    
+    # Move os arquivos para o backup
+    mv "$DIR"/* "$BACKUP_DIR"/
+    
+    echo "Backup concluído em $BACKUP_DIR."
+    echo "Diretório $DIR foi esvaziado."
+fi
+
+
+# -- Arquivo sources.list --
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.backup_"$(date +%Y%m%d%H%M)"
 
-cat <<EOF | sudo tee /etc/apt/sources.list
+sudo tee /etc/apt/sources.list <<'EOF'
 deb http://br.archive.ubuntu.com/ubuntu/ jammy main universe restricted
 deb http://br.archive.ubuntu.com/ubuntu/ jammy-updates main universe restricted
 deb http://br.archive.ubuntu.com/ubuntu/ jammy-security main universe restricted
 EOF
 
+# -- Pacotes do sistema --
 sudo apt update
 sudo apt --fix-broken install
 sudo apt install --reinstall ubuntu-desktop gdm3 -y
